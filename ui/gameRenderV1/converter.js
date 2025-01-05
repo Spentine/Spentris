30fd52a25053cef42be4c4af2fc27c5312467247
Spentine <114318049+Spentine@users.noreply.github.com>
2025-01-04 16:14:18 -0800

    start work on new renderer

---
+++ ui/gameRenderV1/converter.js
@@ -0,0 +1,1 @@
+// converts Stacker object to renderState object
---
+++ ui/gameRenderV1/skin.js
@@ -0,0 +1,157 @@
+import { EventEmitter } from "../../eventEmitter";
+
+// handle skin data
+
+// from previous version
+const tilePositions = {
+  minos_0: {
+    tiles: {
+      Z: {x: 0, y: 0},
+      L: {x: 96, y: 0},
+      O: {x: 192, y: 0},
+      S: {x: 288, y: 0},
+      I: {x: 384, y: 0},
+      J: {x: 0, y: 96},
+      T: {x: 96, y: 96},
+      hold: {x: 192, y: 96},
+      garbage: {x: 288, y: 96},
+      unclearableGarbage: {x: 384, y: 96},
+    },
+    ids: {
+      0: {x: 0, y: 0},
+    }
+  },
+  ghost_0: {
+    tiles: {
+      ghost: {x: 0, y: 0},
+      spawn: {x: 96, y: 0},
+    },
+    ids: {
+      0: {x: 0, y: 0},
+    }
+  },
+  connectedMinos_0: {
+    tiles: {
+      Z: {x: 384, y: 0},
+      L: {x: 768, y: 0},
+      O: {x: 1152, y: 0},
+      S: {x: 1536, y: 0},
+      I: {x: 0, y: 576},
+      J: {x: 96, y: 576},
+      T: {x: 192, y: 576},
+      hold: {x: 288, y: 576},
+      garbage: {x: 1920, y: 0},
+      unclearableGarbage: {x: 1920, y: 576},
+    },
+    ids: {
+      0: {x: 0, y: 0},
+      1: {x: 96, y: 0},
+      2: {x: 192, y: 0},
+      3: {x: 288, y: 0},
+      4: {x: 0, y: 96},
+      5: {x: 96, y: 96},
+      6: {x: 192, y: 96},
+      7: {x: 288, y: 96},
+      8: {x: 0, y: 192},
+      9: {x: 96, y: 192},
+      10: {x: 192, y: 192},
+      11: {x: 288, y: 192},
+      12: {x: 0, y: 288},
+      13: {x: 96, y: 288},
+      14: {x: 192, y: 288},
+      15: {x: 288, y: 288},
+      16: {x: 0, y: 384},
+      17: {x: 96, y: 384},
+      18: {x: 192, y: 384},
+      19: {x: 288, y: 384},
+      20: {x: 0, y: 480},
+      21: {x: 96, y: 480},
+      22: {x: 192, y: 480},
+      23: {x: 288, y: 480},
+    }
+  },
+  connectedGhost_0: {
+    tiles: {
+      ghost: {x: 384, y: 0},
+      spawn: {x: 768, y: 0},
+    },
+    ids: {
+      0: {x: 0, y: 0},
+      1: {x: 96, y: 0},
+      2: {x: 192, y: 0},
+      3: {x: 288, y: 0},
+      4: {x: 0, y: 96},
+      5: {x: 96, y: 96},
+      6: {x: 192, y: 96},
+      7: {x: 288, y: 96},
+      8: {x: 0, y: 192},
+      9: {x: 96, y: 192},
+      10: {x: 192, y: 192},
+      11: {x: 288, y: 192},
+      12: {x: 0, y: 288},
+      13: {x: 96, y: 288},
+      14: {x: 192, y: 288},
+      15: {x: 288, y: 288},
+      16: {x: 0, y: 384},
+      17: {x: 96, y: 384},
+      18: {x: 192, y: 384},
+      19: {x: 288, y: 384},
+      20: {x: 0, y: 480},
+      21: {x: 96, y: 480},
+      22: {x: 192, y: 480},
+      23: {x: 288, y: 480},
+    }
+  },
+};
+
+class TileMap {
+  /**
+   * @param {Object} settings
+   */
+  constructor(settings) {
+    settings = settings ?? {};
+    
+    // name of tilemap
+    this.name = settings.name ?? null;
+    
+    // tilemap type
+    this.type = settings.type ?? null;
+    this.tilePositions = tilePositions[this.type];
+    
+    // image of tilemap
+    this.image = settings.image ?? null;
+    
+    // tilemap image url
+    this.imageURL = settings.imageURL ?? null;
+    
+    // create event emitter
+    this.event = new EventEmitter();
+    
+    // download image if given the url but not the image
+    if (this.imageURL && !this.image) {
+      this.image = new Image();
+      
+      // loaded
+      this.image.addEventListener("load", () => {
+        this.event.emit("load", {
+          time: Date.now(),
+          tilemap: this,
+        });
+      });
+      
+      // fucked
+      this.image.addEventListener("error", (error) => {
+        this.event.emit("loadError", {
+          time: Date.now(),
+          tilemap: this,
+          error: error,
+        });
+      });
+      
+      // start loading
+      this.image.src = this.imageURL;
+    }
+  }
+}
+
+export { TileMap };
